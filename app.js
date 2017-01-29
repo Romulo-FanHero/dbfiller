8ff1707530b7ae079149ae2933495884de6725c5
Romulo-FanHero <romulo@fanhero.com>
2017-01-29 20:16:33 -0200

    funcionarios-ok

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ const maxDate = new Date('2017-01-01');
 const maxUsers = 10000;
 const addrPerUser = 3;
 
+const maxStaff = maxUsers / 10;
+const addrPerStaff = 2;
+
 // Error handler required for pg clients that eventually inside the `pg.Pool`
 // if the connection to the `pg.Server` is reconfigured
 pool.on('error', err => {
@@ -68,7 +71,7 @@ const geraUsuario = () => {
     return {
         nome: escQuotes(faker.name.firstName()),
         sobrenome: escQuotes(faker.name.lastName()),
-        email: faker.internet.email(),
+        email: faker.internet.email().toLowerCase(),
         data_insercao: faker.date.between(minDate, maxDate).toISOString()
     };
 };
@@ -84,14 +87,28 @@ const geraEndereco = () => {
     };
 };
 
+const geraFuncionario = () => {
+    return {
+        nome: escQuotes(faker.name.firstName()),
+        sobrenome: escQuotes(faker.name.lastName()),
+        email_pessoal: faker.internet.email().toLowerCase(),
+        email_empresa: `${faker.internet.userName().toLowerCase()}@cbtu.gov.br`,
+        cpf: faker.random.number({ min: 11111111111, max: 99999999999 }),
+        data_insercao: faker.date.between(minDate, maxDate).toISOString()
+    };
+};
+
 const main = () => {
+
     return clearAll()
+
         .then(result => {
             var len = maxUsers, users = [];
             while (len--) { users.push(geraUsuario()); }
             const qry = squel.insert().into(`${nspc}.usuario`).setFieldsRows(users);
             return simpleQuery2(`${qry} ON CONFLICT DO NOTHING`);
         })
+
         .then(() => {
             console.log(`${maxUsers} usuarios escritos com sucesso`);
             var len = maxUsers * addrPerUser, addrList = [];
@@ -99,13 +116,42 @@ const main = () => {
             const qry = squel.insert().into(`${nspc}.endereco`).setFieldsRows(addrList);
             return simpleQuery2(`${qry} ON CONFLICT DO NOTHING`);
         })
+
         .then(() => {
             console.log(`${maxUsers * addrPerUser} enderecos escritos com sucesso`);
+            var outList = [], endereco_id = 1;
+            for (var i = 1, ilen = addrPerUser; i <= ilen; i++) {
+                for (var j = 1, jlen = maxUsers; j <= jlen; j++) {
+                    outList.push({
+                        usuario_id: j,
+                        funcionario_id: null,
+                        estacao_id: null,
+                        tipo_endereco_id: 0,
+                        endereco_id: endereco_id
+                    });
+                    endereco_id++;
+                }
+            }
+            const qry = squel.insert().into(`${nspc}.lista_endereco`).setFieldsRows(outList);
+            return simpleQuery2(`${qry} ON CONFLICT DO NOTHING`);
+        })
+
+        .then(() => {
+            console.log(`${maxUsers * addrPerUser} usuarios x enderecos escritos com sucesso`);
+            var len = maxStaff, outList = [];
+            while (len--) { outList.push(geraFuncionario()); }
+            const qry = squel.insert().into(`${nspc}.funcionario`).setFieldsRows(outList);
+            return simpleQuery2(`${qry} ON CONFLICT DO NOTHING`);
+        })
+
+        .then(() => {
+            console.log(`${maxStaff} funcionarios escritos com sucesso`);
             process.exit();
         })
         .catch(err => {
             console.error(err);
         });
+
 };
 
 main();
