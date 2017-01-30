52bad8022ff45a61ad1e9a0bf199e880062f881d
Romulo-FanHero <romulo@fanhero.com>
2017-01-30 10:25:55 -0200

    larger-size-and-wider-date-ranges

--- app.js
+++ app.js
@@ -18,14 +18,26 @@ const pool = new (require('pg').Pool)({
 
 const nspc = 'cadastro';
 
-const minDate = new Date('2014-01-01');
-const maxDate = new Date('2017-01-01');
+const userMinDate = new Date('2014-01-01');
+const userMaxDate = new Date(Date.now());
 
-const maxUsers = 10000;
-const addrPerUser = 3;
+const maxUsers = 200000;
+const addrPerUser = 4;
 
 const maxStaff = maxUsers / 10;
-const addrPerStaff = 2;
+const addrPerStaff = 3;
+
+const staffMinDate = new Date('1985-01-01');
+const staffMaxDate = new Date('2017-01-01');
+
+const maxStations = maxUsers / 100;
+const addrPerStation = 2;
+
+const staffPerStation = maxStaff / maxStations;
+const managersPerStation = 2;
+
+const maxManagers = maxStations * 2;
+const managersPerStaff = maxStaff / maxManagers;
 
 // Error handler required for pg clients that eventually inside the `pg.Pool`
 // if the connection to the `pg.Server` is reconfigured
@@ -72,7 +84,7 @@ const geraUsuario = () => {
         nome: escQuotes(faker.name.firstName()),
         sobrenome: escQuotes(faker.name.lastName()),
         email: faker.internet.email().toLowerCase(),
-        data_insercao: faker.date.between(minDate, maxDate).toISOString()
+        data_insercao: faker.date.between(userMinDate, userMaxDate).toISOString()
     };
 };
 
@@ -94,7 +106,13 @@ const geraFuncionario = () => {
         email_pessoal: faker.internet.email().toLowerCase(),
         email_empresa: `${faker.internet.userName().toLowerCase()}@cbtu.gov.br`,
         cpf: faker.random.number({ min: 11111111111, max: 99999999999 }),
-        data_insercao: faker.date.between(minDate, maxDate).toISOString()
+        data_insercao: faker.date.between(staffMinDate, staffMaxDate).toISOString()
+    };
+};
+
+const geraEstacao = () => {
+    return {
+        nome: escQuotes(faker.address.county())
     };
 };
 
@@ -177,13 +195,83 @@ const main = () => {
             return simpleQuery2(`${qry} ON CONFLICT DO NOTHING`);
         })
 
-
+        // PREENCHE ESTACOES
         .then(() => {
             console.log(`${maxStaff * addrPerStaff} funcionarios x enderecos escritos com sucesso`);
+            var len = maxStations, outList = [];
+            while (len--) { outList.push(geraEstacao()); }
+            const qry = squel.insert().into(`${nspc}.estacao`).setFieldsRows(outList);
+            return simpleQuery2(`${qry} ON CONFLICT DO NOTHING`);
+        })
+
+        // PREENCHE ENDERECOS DE ESTACOES
+        .then(() => {
+            console.log(`${maxStations} estacoes escritas com sucesso`);
+            var len = (maxStations * addrPerStation), addrList = [];
+            while (len--) { addrList.push(geraEndereco()); }
+            const qry = squel.insert().into(`${nspc}.endereco`).setFieldsRows(addrList);
+            return simpleQuery2(`${qry} ON CONFLICT DO NOTHING`);
+        })
+
+        // PREENCHE RELACIONAMENTO ESTACOES X ENDERECOS
+        .then(() => {
+            console.log(`${maxStations * addrPerStation} enderecos de estacoes escritos com sucesso`);
+            var outList = [], endereco_id = (maxUsers * addrPerUser) + (maxStaff * addrPerStaff) + 1;
+            for (var i = 1, ilen = addrPerStation; i <= ilen; i++) {
+                for (var j = 1, jlen = maxStations; j <= jlen; j++) {
+                    outList.push({
+                        usuario_id: null,
+                        funcionario_id: null,
+                        estacao_id: j,
+                        tipo_endereco_id: 2,
+                        endereco_id: endereco_id
+                    });
+                    endereco_id++;
+                }
+            }
+            const qry = squel.insert().into(`${nspc}.lista_endereco`).setFieldsRows(outList);
+            return simpleQuery2(`${qry} ON CONFLICT DO NOTHING`);
+        })
+
+        // PREENCHE RELACIONAMENTO FUNCIONARIOS X ESTACOES
+        .then(() => {
+            console.log(`${maxStations * addrPerStation} estacoes x enderecos escritos com sucesso`);
+            var outList = [], estacao_id = 1;
+            for (var i = 1, len = maxStaff; i <= len; i++) {
+                outList.push({
+                    funcionario_id: i,
+                    estacao_id: estacao_id
+                });
+                estacao_id++;
+                if (estacao_id > maxStations) estacao_id = 1;
+            }
+            const qry = squel.insert().into(`${nspc}.lista_alocacao`).setFieldsRows(outList);
+            return simpleQuery2(`${qry} ON CONFLICT DO NOTHING`);
+        })
+
+        // PREENCHE RELACIONAMENTO FUNCIONARIOS GERENTES X ESTACOES
+        .then(() => {
+            console.log(`${maxStaff} funcionarios x estacoes escritos com sucesso`);
+            var outList = [], estacao_id = 1;
+            for (var i = 1, len = maxManagers; i <= len; i++) {
+                outList.push({
+                    funcionario_id: i,
+                    estacao_id: estacao_id
+                });
+                estacao_id++;
+                if (estacao_id > maxStations) estacao_id = 1;
+            }
+            const qry = squel.insert().into(`${nspc}.lista_gerencia`).setFieldsRows(outList);
+            return simpleQuery2(`${qry} ON CONFLICT DO NOTHING`);
+        })
+
+        .then(() => {
+            console.log(`${maxManagers} funcionarios gerentes x estacoes escritos com sucesso`);
             process.exit();
         })
         .catch(err => {
             console.error(err);
+            process.exit();
         });
 
 };
